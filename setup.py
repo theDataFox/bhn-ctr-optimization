83f152d423179647628a7c128229e59e9a84b082
theDataFox <leejoshijones@gmail.com>
2019-07-20 20:08:02 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting and optimization CTR for top of funnel for websites',
+    author='Lee Joshi-Jones',
+    license='MIT',
+)
